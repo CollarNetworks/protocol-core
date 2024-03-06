196e52140d64444caf3329bb30b210510c54d286
Caleb <ditchfieldcaleb@gmail.com>
2024-03-06 14:23:15 -0800

    ICollarVaultManager refactor complete

--- src/interfaces/ICollarVaultManager.sol
+++ src/interfaces/ICollarVaultManager.sol
@@ -12,19 +12,39 @@ import { IERC6909WithSupply } from "../interfaces/IERC6909WithSupply.sol";
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 
 abstract contract ICollarVaultManager is IERC6909WithSupply, Ownable {
+
+    // ----- IMMUTABLES ----- //
+
     address public immutable user;
     address public immutable engine;
+
+    // ----- STATE VARIABLES ----- //
 
     uint256 public vaultCount;
 
     mapping(bytes32 uuid => CollarVaultState.Vault vault) internal vaultsByUUID;
     mapping(bytes32 => uint256) public vaultTokenCashSupply;
 
+    // ----- CONSTRUCTOR ----- //
+
     constructor(address _engine, address _owner) Ownable(_owner) {
         user = _owner;
         engine = _engine;
         vaultCount = 0;
     }
+
+    // ----- VIEW FUNCTIONS ----- //
+
+    /// @notice Get the entire vault state as bytes
+    /// @param uuid UUID of the vault to get state for
+    function vaultInfo(bytes32 uuid) external view virtual returns (bytes calldata data);
+
+    /// @notice Preview the cash amount redeemable for a given amount of tokens for a particular vault
+    /// @param uuid UUID of the vault to redeem from
+    /// @param amount Amount of tokens to redeem
+    function previewRedeem(bytes32 uuid, uint256 amount) external virtual returns (uint256);
+
+    // ----- STATE CHANGING FUNCTIONS ----- //
 
     /// @notice Opens a new vault
     /// @param assets Data about the assets in the vault & amounts of each
@@ -45,15 +65,8 @@ abstract contract ICollarVaultManager is IERC6909WithSupply, Ownable {
     /// @param amount Amount of tokens to redeem
     function redeem(bytes32 uuid, uint256 amount) external virtual;
 
-    /// @notice Preview the cash amount redeemable for a given amount of tokens
-    /// @param uuid UUID of the vault to redeem from
-    /// @param amount Amount of tokens to redeem
-    function previewRedeem(bytes32 uuid, uint256 amount) external virtual returns (uint256);
-
     /// @notice Withdraws cash from a vault loan
     /// @param uuid UUID of the vault to withdraw from
     /// @param amount Amount of cash to withdraw
     function withdraw(bytes32 uuid, uint256 amount) external virtual;
-
-    function vaultInfo(bytes32 uuid) external view virtual returns (bytes calldata data);
 }
