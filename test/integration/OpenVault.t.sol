05bfdfcfee4f491349a297f00411efe3edde292a
Caleb <ditchfieldcaleb@gmail.com>
2024-04-16 00:22:55 -0700

    Bug finally fixed

--- test/integration/OpenVault.t.sol
+++ test/integration/OpenVault.t.sol
@@ -22,18 +22,23 @@ import { ISwapRouter } from "@uni-v3-periphery/interfaces/ISwapRouter.sol";
 // UniversalRouter  - - - - - - - - 0xec7BE89e9d109e7e3Fec59c222CF297125FEFda2
 // NonFungiblePositionManager - - - 0xC36442b4a4522E871399CD717aBDD847Ab11FE88
 // TickLens - - - - - - - - - - - - 0xbfd8137f7d1516D3ea5cA83523914859ec47F573
-// WETH - - - - - - - - - - - - - - 0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619
+// WMatic - - - - - - - - - - - - - - 0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619
 // USDC - - - - - - - - - - - - - - 0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359
 
 contract CollarOpenVaultIntegrationTest is Test {
     address user = makeAddr("user1");       // the person who will be opening a vault
     address provider = makeAddr("user2");   // the person who will be providing liquidity
     address swapRouterAddress = address(0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45);
-    address WETHAddress = address(0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619);
+    address WMaticAddress = address(0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270);
     address USDCAddress = address(0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359);
 
-    IERC20 WETH = IERC20(WETHAddress);
+    IERC20 WMatic = IERC20(WMaticAddress);
     IERC20 USDC = IERC20(USDCAddress);
+
+    // We have to impersonate a top holder of USDC, because `vm.deal` oddly fails when trying to cheat a balance here
+    // It seems to be because `vm.deal` can't find the storage slot when it comes to the FiatTokenProxy contract type
+    // (it works fine with the UERC20ChildProxy type, which is what most other proxy-tokens use!)
+    address USDCTopHolder = address(0x2C2301FDB0bfA06EAABaA0122CbCEb2265337C25);
 
     ISwapRouter swapRouter = ISwapRouter(swapRouterAddress);
 
@@ -50,31 +55,31 @@ contract CollarOpenVaultIntegrationTest is Test {
         - engine: address of the CollarEngine
         - tickScaleFactor: 100 (1 tick = 1%)
         - cashAsset: USDC
-        - collateralAsset: WETH
+        - collateralAsset: WMatic
         - duration: 1 day
         - ltv: 9000 (90%)
     5. We add the pool to the engine
-    6. We give the user USDC and WETH, as well as the liquidity provider
+    6. We give the user USDC and WMatic, as well as the liquidity provider
     7. WE label existing addresses for better test output
     8. We deploy a vault manager for the user
-    9. We approve the vault manager to spend the user's USDC and WETH
-    10. We approve the pool to spend the liquidity provider's USDC and WETH
+    9. We approve the vault manager to spend the user's USDC and WMatic
+    10. We approve the pool to spend the liquidity provider's USDC and WMatic
     11. We add liquidity to the pool in ticks 110 through 115 (as the liquidity provider)
 
     */
 
     function setUp() public {
         string memory forkRPC = vm.envString("POLYGON_MAINNET_RPC");
-        vm.createSelectFork(forkRPC, 55000000);
-        assertEq(block.number, 55000000);
+        vm.createSelectFork(forkRPC, 55850000);
+        assertEq(block.number, 55850000);
 
         engine = new CollarEngine(swapRouterAddress);
         engine.addLTV(9000);
 
-        pool = new CollarPool(address(engine), 100, USDCAddress, WETHAddress, 1 days, 9000);
+        pool = new CollarPool(address(engine), 100, USDCAddress, WMaticAddress, 1 days, 9000);
     
         engine.addSupportedCashAsset(USDCAddress);
-        engine.addSupportedCollateralAsset(WETHAddress);
+        engine.addSupportedCollateralAsset(WMaticAddress);
         engine.addCollarDuration(1 days);
         engine.addLiquidityPool(address(pool));
 
@@ -84,53 +89,60 @@ contract CollarOpenVaultIntegrationTest is Test {
         vm.label(address(pool), "POOL");
         vm.label(swapRouterAddress, "SWAP ROUTER 02");
         vm.label(USDCAddress, "USDC");
-        vm.label(WETHAddress, "WETH");
+        vm.label(WMaticAddress, "WMatic");
 
-        deal(USDCAddress, user, 100_000 ether);
-        deal(WETHAddress, user, 100_000 ether);
-        deal(USDCAddress, provider, 100_000 ether);
-        deal(WETHAddress, provider, 100_000 ether);
+        deal(WMaticAddress, user, 100_000 ether);
+        deal(WMaticAddress, provider, 100_000 ether);
+
+        // see comment above about the `vm.deal` issue
+        //deal(USDCAddress, user, 100_000 ether);
+        //deal(USDCAddress, provider, 100_000 ether);
+
+        startHoax(USDCTopHolder);
+
+        USDC.transfer(user, 100_000e6);
+        USDC.transfer(provider, 100_000e6);
 
         startHoax(user);
 
         vaultManager = CollarVaultManager(engine.createVaultManager());
-        USDC.approve(address(vaultManager), 100_000 ether);
-        WETH.approve(address(vaultManager), 100_000 ether);
+        USDC.approve(address(vaultManager), 100_000e6);
+        WMatic.approve(address(vaultManager), 100_000 ether);
 
         startHoax(provider);
 
-        USDC.approve(address(pool), 100_000 ether);
-        WETH.approve(address(pool), 100_000 ether);
+        USDC.approve(address(pool), 100_000e6);
+        WMatic.approve(address(pool), 100_000 ether);
 
-        pool.addLiquidityToSlot(110, 10_000 ether);
-        pool.addLiquidityToSlot(111, 11_000 ether);
-        pool.addLiquidityToSlot(112, 12_000 ether);
-        pool.addLiquidityToSlot(113, 13_000 ether);
-        pool.addLiquidityToSlot(114, 12_000 ether);
-        pool.addLiquidityToSlot(115, 11_000 ether);
+        pool.addLiquidityToSlot(110, 10_000e6);
+        pool.addLiquidityToSlot(111, 11_000e6);
+        pool.addLiquidityToSlot(112, 12_000e6);
+        pool.addLiquidityToSlot(113, 13_000e6);
+        pool.addLiquidityToSlot(114, 12_000e6);
+        pool.addLiquidityToSlot(115, 11_000e6);
 
         vm.stopPrank();
 
         assertEq(engine.isValidCollarDuration(1 days), true);        
         assertEq(engine.isValidLTV(9000), true);
         assertEq(engine.isSupportedCashAsset(USDCAddress), true);
-        assertEq(engine.isSupportedCollateralAsset(WETHAddress), true);
+        assertEq(engine.isSupportedCollateralAsset(WMaticAddress), true);
         assertEq(engine.addressToVaultManager(user), address(vaultManager));
         assertEq(engine.supportedLiquidityPoolsLength(), 1);
         assertEq(engine.isSupportedLiquidityPool(address(pool)), true);
     
-        assertEq(pool.getLiquidityForSlot(110), 10_000 ether);
-        assertEq(pool.getLiquidityForSlot(111), 11_000 ether);
-        assertEq(pool.getLiquidityForSlot(112), 12_000 ether);
-        assertEq(pool.getLiquidityForSlot(113), 13_000 ether);
-        assertEq(pool.getLiquidityForSlot(114), 12_000 ether);
-        assertEq(pool.getLiquidityForSlot(115), 11_000 ether);
+        assertEq(pool.getLiquidityForSlot(110), 10_000e6);
+        assertEq(pool.getLiquidityForSlot(111), 11_000e6);
+        assertEq(pool.getLiquidityForSlot(112), 12_000e6);
+        assertEq(pool.getLiquidityForSlot(113), 13_000e6);
+        assertEq(pool.getLiquidityForSlot(114), 12_000e6);
+        assertEq(pool.getLiquidityForSlot(115), 11_000e6);
     }
 
     function test_openVault() public {
         ICollarVaultState.AssetSpecifiers memory assets = ICollarVaultState.AssetSpecifiers({
-            collateralAsset: WETHAddress,
-            collateralAmount: 1 ether,
+            collateralAsset: WMaticAddress,
+            collateralAmount: 1000 ether,
             cashAsset: USDCAddress,
             cashAmount: 100e6
         });
@@ -161,21 +173,21 @@ contract CollarOpenVaultIntegrationTest is Test {
         assertEq(vault.ltv, 9000);
 
         // check asset specific info
-        assertEq(vault.collateralAsset, WETHAddress);
+        assertEq(vault.collateralAsset, WMaticAddress);
         assertEq(vault.cashAsset, USDCAddress);
         assertEq(vault.collateralAmount, 100 ether);
-        assertEq(vault.cashAmount, 100 ether);
+        assertEq(vault.cashAmount, 100e6);
 
         // check liquidity pool stuff
         assertEq(vault.liquidityPool, address(pool));
-        assertEq(vault.lockedPoolCash, 10 ether);
+        assertEq(vault.lockedPoolCash, 10e6);
         assertEq(vault.putStrikeTick, 90);
         assertEq(vault.callStrikeTick, 110);
         assertEq(vault.putStrikePrice, 0.9e18);
         assertEq(vault.callStrikePrice, 1.1e18);
 
         // check vault specific stuff
-        assertEq(vault.loanBalance, 90 ether);
-        assertEq(vault.lockedVaultCash, 10 ether);
+        assertEq(vault.loanBalance, 90e6);
+        assertEq(vault.lockedVaultCash, 10e6);
     }
 }
