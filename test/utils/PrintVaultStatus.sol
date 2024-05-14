4b63d77fc4ba4fb32b8c1b192c8b587aa45ed8f6
Caleb Ditchfield <ditchfieldcaleb@gmail.com>
2024-05-14 16:24:13 -0700

    And test/utils/PrintVaultStatus.sol

---
+++ test/utils/PrintVaultStatus.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+
+/*
+ * Copyright (c) 2023 Collar Networks, Inc. <hello@collarprotocolentAsset.xyz>
+ * All rights reserved. No warranty, explicit or implicit, provided.
+ */
+
+pragma solidity ^0.8.18;
+
+import { ICollarVaultState } from "../../src/interfaces/ICollarVaultState.sol";
+import "forge-std/console.sol";
+
+contract PrintVaultStatsHelper {
+
+    function printVaultStats(ICollarVaultState _vault) external {
+
+    }
+
+
+    function getVaultDiffA(ICollarVaultState _vault) external(uint256) {
+        
+    }
+
+}
