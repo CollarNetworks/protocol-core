748d7c67fd856267f7510fa99a39e2829f6dedb6
Carlos D. Di Matteo <carlosdima97@gmail.com>
2024-06-14 12:38:48 -0500

    try to create a pool with USDT

---
+++ src/interfaces/external/IWETH9.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+
+/*
+ * Copyright (c) 2023 Collar Networks, Inc. <hello@collarprotocolentAsset.xyz>
+ * All rights reserved. No warranty, explicit or implicit, provided.
+ */
+
+interface IWETH9 {
+    event Approval(address indexed src, address indexed guy, uint256 wad);
+    event Transfer(address indexed src, address indexed dst, uint256 wad);
+    event Deposit(address indexed dst, uint256 wad);
+    event Withdrawal(address indexed src, uint256 wad);
+
+    function balanceOf(address owner) external view returns (uint256);
+
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    function deposit() external payable;
+
+    function withdraw(uint256 wad) external;
+
+    function totalSupply() external view returns (uint256);
+
+    function approve(address guy, uint256 wad) external returns (bool);
+
+    function transfer(address dst, uint256 wad) external returns (bool);
+
+    function transferFrom(address src, address dst, uint256 wad) external returns (bool);
+}
