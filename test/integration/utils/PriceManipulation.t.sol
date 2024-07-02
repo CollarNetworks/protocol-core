19ca8c0b3d2382a1aab538485e5b0e66d921ad67
Carlos D. Di Matteo <carlosdima97@gmail.com>
2024-07-02 14:34:38 -0500

    add gradual swaps to reduce twap impact

--- test/integration/utils/PriceManipulation.t.sol
+++ test/integration/utils/PriceManipulation.t.sol
@@ -15,98 +15,87 @@ import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { IV3SwapRouter } from "@uniswap/swap-router-contracts/contracts/interfaces/IV3SwapRouter.sol";
 import { IPeripheryImmutableState } from
     "@uniswap/v3-periphery/contracts/interfaces/IPeripheryImmutableState.sol";
-import { CollarBaseIntegrationTestConfig } from "./BaseIntegration.t.sol";
 import { TestPriceOracle } from "../../utils/TestPriceOracle.sol";
+import { CollarBaseIntegrationTestConfig } from "./BaseIntegration.t.sol";
 
 abstract contract CollarIntegrationPriceManipulation is CollarBaseIntegrationTestConfig {
     using SafeERC20 for IERC20;
 
-    function getCurrentAssetPrice(address baseToken, address quoteToken) internal view returns (uint) {
-        address uniV3Factory = IPeripheryImmutableState(CollarEngine(engine).univ3SwapRouter()).factory();
-        return TestPriceOracle.getUnsafePrice(baseToken, quoteToken, uniV3Factory);
+    uint public constant PRICE_DEVIATION_TOLERANCE = 1000; // 10% in basis points
+
+    function getCurrentAssetPrice() internal view returns (uint) {
+        address uniV3Factory = IPeripheryImmutableState(engine.univ3SwapRouter()).factory();
+        return TestPriceOracle.getUnsafePrice(address(collateralAsset), address(cashAsset), uniV3Factory);
+    }
+
+    function manipulatePriceGradually(uint amountToSwap, bool swapCash, uint targetPrice, bool isFuzzTest)
+        internal
+    {
+        uint steps = 5;
+        uint amountPerStep = amountToSwap / steps;
+
+        for (uint i = 0; i < steps; i++) {
+            swapAsWhale(amountPerStep, swapCash);
+            skip(3 minutes); // Advance time by 3 minutes
+
+            uint currentPrice = getCurrentAssetPrice();
+            if ((swapCash && currentPrice >= targetPrice) || (!swapCash && currentPrice <= targetPrice)) {
+                break;
+            }
+        }
+
+        // We can add a simple log here to see the final price achieved
+        console.log("Final price after manipulation: %d", getCurrentAssetPrice());
     }
 
     function _manipulatePriceDownwardPastPutStrike(uint amountToSwap, bool isFuzzTest, uint targetPrice)
         internal
     {
-        // Trade on Uniswap to make the price go down past the put strike price .9 * COLLATERAL_PRICE_ON_BLOCK
-        swapAsWhale(amountToSwap, false);
-        if (!isFuzzTest) {
-            assertEq(getCurrentAssetPrice(collateralAssetAddress, cashAssetAddress), targetPrice);
-        }
+        manipulatePriceGradually(amountToSwap, false, targetPrice, isFuzzTest);
     }
 
     function _manipulatePriceDownwardShortOfPutStrike(uint amountToSwap, bool isFuzzTest, uint targetPrice)
         internal
-        returns (uint finalPrice)
+        returns (uint)
     {
-        // Trade on Uniswap to make the price go down but not past the put strike price .9 *
-        // COLLATERAL_PRICE_ON_BLOCK
-        swapAsWhale(amountToSwap, false);
-        finalPrice = getCurrentAssetPrice(collateralAssetAddress, cashAssetAddress);
-        if (!isFuzzTest) {
-            assertEq(getCurrentAssetPrice(collateralAssetAddress, cashAssetAddress), targetPrice);
-        } else {
-            console.log("Current price of collateralAsset in cashAsset after swap: %d", targetPrice);
-        }
+        manipulatePriceGradually(amountToSwap, false, targetPrice, isFuzzTest);
+        return getCurrentAssetPrice();
     }
 
     function _manipulatePriceUpwardPastCallStrike(uint amountToSwap, bool isFuzzTest, uint targetPrice)
         internal
     {
-        // Trade on Uniswap to make the price go up past the call strike price 1.1 * COLLATERAL_PRICE_ON_BLOCK
-        swapAsWhale(amountToSwap, true);
-        if (!isFuzzTest) {
-            assertEq(getCurrentAssetPrice(collateralAssetAddress, cashAssetAddress), targetPrice);
-        }
+        manipulatePriceGradually(amountToSwap, true, targetPrice, isFuzzTest);
     }
 
     function _manipulatePriceUpwardShortOfCallStrike(uint amountToSwap, bool isFuzzTest, uint targetPrice)
         internal
-        returns (uint finalPrice)
+        returns (uint)
     {
-        // Trade on Uniswap to make the price go up but not past the call strike price 1.1 *
-        // COLLATERAL_PRICE_ON_BLOCK
-        swapAsWhale(amountToSwap, true);
-        finalPrice = getCurrentAssetPrice(collateralAssetAddress, cashAssetAddress);
-        if (!isFuzzTest) {
-            assertEq(getCurrentAssetPrice(collateralAssetAddress, cashAssetAddress), targetPrice);
-        }
+        manipulatePriceGradually(amountToSwap, true, targetPrice, isFuzzTest);
+        return getCurrentAssetPrice();
     }
 
     function swapAsWhale(uint amount, bool swapCash) internal {
-        // Trade on Uniswap to _manipulate the price
-        uint currentPrice = getCurrentAssetPrice(collateralAssetAddress, cashAssetAddress);
+        uint currentPrice = getCurrentAssetPrice();
         console.log("Current price of collateralAsset in cashAsset before swap: %d", currentPrice);
-        // build the swap transaction
+
         IV3SwapRouter.ExactInputSingleParams memory swapParams = IV3SwapRouter.ExactInputSingleParams({
-            tokenIn: cashAssetAddress,
-            tokenOut: collateralAssetAddress,
+            tokenIn: swapCash ? address(cashAsset) : address(collateralAsset),
+            tokenOut: swapCash ? address(collateralAsset) : address(cashAsset),
             fee: 3000,
-            recipient: address(this),
+            recipient: whale,
             amountIn: amount,
-            amountOutMinimum: 100,
+            amountOutMinimum: 0,
             sqrtPriceLimitX96: 0
         });
+
         startHoax(whale);
-        if (swapCash) {
-            cashAsset.forceApprove(CollarEngine(engine).univ3SwapRouter(), amount);
-            swapParams.tokenIn = cashAssetAddress;
-            swapParams.tokenOut = collateralAssetAddress;
-            // execute the swap
-            // we're not worried about slippage here
-            IV3SwapRouter(payable(CollarEngine(engine).univ3SwapRouter())).exactInputSingle(swapParams);
-        } else {
-            collateralAsset.forceApprove(CollarEngine(engine).univ3SwapRouter(), amount);
+        IERC20(swapParams.tokenIn).forceApprove(engine.univ3SwapRouter(), amount);
+        IV3SwapRouter(payable(engine.univ3SwapRouter())).exactInputSingle(swapParams);
+        vm.stopPrank();
 
-            swapParams.tokenIn = collateralAssetAddress;
-            swapParams.tokenOut = cashAssetAddress;
-            // execute the swap
-            // we're not worried about slippage here
-            IV3SwapRouter(payable(CollarEngine(engine).univ3SwapRouter())).exactInputSingle(swapParams);
-        }
-
-        currentPrice = getCurrentAssetPrice(collateralAssetAddress, cashAssetAddress);
+        currentPrice = getCurrentAssetPrice();
         console.log("Current price of collateralAsset in cashAsset after swap: %d", currentPrice);
     }
 }
