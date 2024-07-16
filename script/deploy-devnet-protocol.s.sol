d485230f093790907e2e60672ef36464f1252c54
Carlos D. Di Matteo <carlosdima97@gmail.com>
2024-07-16 15:43:17 -0500

    clean up scripts (broken until new prs are merged)

--- script/deploy-devnet-protocol.s.sol
+++ script/deploy-devnet-protocol.s.sol
@@ -4,9 +4,6 @@ pragma solidity 0.8.22;
 import "forge-std/Script.sol";
 import "forge-std/console.sol";
 
-import { ICollarVaultState } from "../src/interfaces/ICollarVaultState.sol";
-import { CollarPool } from "../src/implementations/CollarPool.sol";
-import { CollarVaultManager } from "../src/implementations/CollarVaultManager.sol";
 import { CollarEngine } from "../src/implementations/CollarEngine.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
@@ -104,129 +101,14 @@ contract DeployInitializedDevnetProtocol is Script {
         console.log(" - Engine - - - - - - - ", engine);
     }
 
-    function _createPools() internal returns (address[numOfPools] memory pools) {
-        // create main WETH pools
-        address fiveMin90ltvPool = address(new CollarPool(engine, 1, USDC, WETH, 5 minutes, 9000));
-        address fiveMin90LTVTetherPool = address(new CollarPool(engine, 1, USDT, WETH, 5 minutes, 9000));
-        address fiveMin50LTVPool = address(new CollarPool(engine, 1, USDC, WETH, 5 minutes, 5000));
-        address oneMonth90LTVPool = address(new CollarPool(engine, 1, USDC, WETH, 30 days, 9000));
-        address oneMonth50LTVPool = address(new CollarPool(engine, 1, USDC, WETH, 30 days, 5000));
-        address oneYear90LTVPool = address(new CollarPool(engine, 1, USDC, WETH, 12 * 30 days, 9000));
-        address oneYear50LTVPool = address(new CollarPool(engine, 1, USDC, WETH, 12 * 30 days, 5000));
-
-        CollarEngine(engine).addLiquidityPool(fiveMin90ltvPool);
-        CollarEngine(engine).addLiquidityPool(fiveMin90LTVTetherPool);
-        CollarEngine(engine).addLiquidityPool(fiveMin50LTVPool);
-        CollarEngine(engine).addLiquidityPool(oneMonth90LTVPool);
-        CollarEngine(engine).addLiquidityPool(oneMonth50LTVPool);
-        CollarEngine(engine).addLiquidityPool(oneYear90LTVPool);
-        CollarEngine(engine).addLiquidityPool(oneYear50LTVPool);
-
-        // rest of pools all 5 minutes with usdc
-
-        address fiveMin90LTVWBTCPool = address(new CollarPool(engine, 1, USDC, WBTC, 5 minutes, 9000));
-        address fiveMin90LTVMATICPool = address(new CollarPool(engine, 1, USDC, MATIC, 5 minutes, 9000));
-        address fiveMin90LTVstETHPool = address(new CollarPool(engine, 1, USDC, stETH, 5 minutes, 9000));
-        address fiveMin90LTVeETHPool = address(new CollarPool(engine, 1, USDC, eETH, 5 minutes, 9000));
-
-        CollarEngine(engine).addLiquidityPool(fiveMin90LTVWBTCPool);
-        CollarEngine(engine).addLiquidityPool(fiveMin90LTVMATICPool);
-        CollarEngine(engine).addLiquidityPool(fiveMin90LTVstETHPool);
-        CollarEngine(engine).addLiquidityPool(fiveMin90LTVeETHPool);
-        console.log(" - Collar 5 minutes 90LTV WETH/USDC Pool - - - - - - - ", fiveMin90ltvPool);
-        console.log(" - Collar 5 minutes 90LTV USDT/WETH Pool - - - - - - - ", fiveMin90LTVTetherPool);
-        console.log(" - Collar 5 minutes 50LTV WETH/USDC Pool - - - - - - - ", fiveMin50LTVPool);
-        console.log(" - Collar 30 days 90LTV WETH/USDC Pool - - - - - - - - ", oneMonth90LTVPool);
-        console.log(" - Collar 30 days 50LTV WETH/USDC Pool - - - - - - - - ", oneMonth50LTVPool);
-        console.log(" - Collar 12 months 90LTV WETH/USDC Pool - - - - - - - ", oneYear90LTVPool);
-        console.log(" - Collar 12 months 50LTV WETH/USDC Pool - - - - - - - ", oneYear50LTVPool);
-        console.log(" - Collar 5 minutes 90LTV WBTC/USDC Pool - - - - - - - ", fiveMin90LTVWBTCPool);
-        console.log(" - Collar 5 minutes 90LTV MATIC/USDC Pool - - - - - - - ", fiveMin90LTVMATICPool);
-        console.log(" - Collar 5 minutes 90LTV stETH/USDC Pool - - - - - - - ", fiveMin90LTVstETHPool);
-        console.log(" - Collar 5 minutes 90LTV eETH/USDC Pool - - - - - - - ", fiveMin90LTVeETHPool);
-        pools = [
-            fiveMin90ltvPool,
-            fiveMin90LTVTetherPool,
-            fiveMin50LTVPool,
-            oneMonth90LTVPool,
-            oneMonth50LTVPool,
-            oneYear90LTVPool,
-            oneYear50LTVPool,
-            fiveMin90LTVWBTCPool,
-            fiveMin90LTVMATICPool,
-            fiveMin90LTVstETHPool,
-            fiveMin90LTVeETHPool
-        ];
-        require(
-            pools.length == numOfPools,
-            "config error: number of pools created does not match the number of pools expected"
-        );
-    }
-
-    function _verifyVaultManagerCreation(address user1, address user2) internal {
-        vm.startBroadcast(user1);
-
-        address user1VaultManager = address(CollarEngine(engine).createVaultManager());
-
-        vm.stopBroadcast();
-
-        vm.startBroadcast(user2);
-
-        address user2VaultManager = address(CollarEngine(engine).createVaultManager());
-
-        vm.stopBroadcast();
-
-        require(CollarEngine(engine).addressToVaultManager(user1) == user1VaultManager);
-        require(CollarEngine(engine).addressToVaultManager(user2) == user2VaultManager);
-        console.log("\n # Vault Managers\n");
-        console.log(" - User 1 Vault Manager: ", user1VaultManager);
-        console.log(" - User 2 Vault Manager: ", user2VaultManager);
-    }
-
-    function _verifyPoolCreation(uint expectedLength, address firstPool, address lastPool) internal view {
-        // supportedLiquidityPoolsLength
-        uint shouldBePoolLength = CollarEngine(engine).supportedLiquidityPoolsLength();
-        console.log(" shouldBePoolLength", shouldBePoolLength);
-        require(shouldBePoolLength == expectedLength);
-
-        // getSupportedLiquidityPool
-        address shouldBeFirstPoolCreated = CollarEngine(engine).getSupportedLiquidityPool(0);
-        console.log(" shouldBeFirstPoolCreated", shouldBeFirstPoolCreated);
-        require(shouldBeFirstPoolCreated == firstPool);
-
-        address shouldBeLastPoolCreated =
-            CollarEngine(engine).getSupportedLiquidityPool(shouldBePoolLength - 1);
-        console.log(" shouldBeLastPoolCreated", shouldBeLastPoolCreated);
-        require(shouldBeLastPoolCreated == lastPool);
-    }
-
-    function _addLiquidityToPools(address[numOfPools] memory pools, uint amountToAdd) internal {
-        // setup pool liquidity // assume provider has enough funds
-
-        // add liquidity to each pool
-        for (uint i = 0; i < pools.length; i++) {
-            address cashAssetToUse = CollarPool(pools[i]).cashAsset();
-            IERC20(cashAssetToUse).forceApprove(pools[i], amountToAdd * 5);
-            CollarPool(pools[i]).addLiquidityToSlot(11_100, amountToAdd);
-            CollarPool(pools[i]).addLiquidityToSlot(11_200, amountToAdd);
-            CollarPool(pools[i]).addLiquidityToSlot(11_500, amountToAdd);
-            CollarPool(pools[i]).addLiquidityToSlot(12_000, amountToAdd);
-        }
-    }
-
     function run() external {
         require(chainId == block.chainid, "chainId does not match the chainId in config");
         (address deployer, address user1, address user2, address liquidityProvider) = setup();
         vm.startBroadcast(deployer);
         _deployandSetupEngine();
-        // create main WETH pools
-        address[numOfPools] memory createdPools = _createPools();
         vm.stopBroadcast();
-
         console.log("\n");
         console.log("Verifying deployment : ");
-        _verifyVaultManagerCreation(user1, user2);
-        _verifyPoolCreation(numOfPools, createdPools[0], createdPools[numOfPools - 1]);
 
         /**
          * @dev in order for this part to work provider address needs to be funded with casdh assets through
@@ -234,11 +116,5 @@ contract DeployInitializedDevnetProtocol is Script {
          */
         require(liquidityProvider != address(0), "liquidity provider address not set");
         require(liquidityProvider.balance > 1000, "liquidity provider address not funded");
-        uint amountToAdd = 100_000e6;
-        uint lpBalance = IERC20(CollarPool(createdPools[0]).cashAsset()).balanceOf(liquidityProvider);
-        require(lpBalance >= amountToAdd * 4, "liquidity provider does not have enough funds");
-        vm.startBroadcast(liquidityProvider);
-        _addLiquidityToPools(createdPools, amountToAdd);
-        vm.stopBroadcast();
     }
 }
