9a406a7a3ed05bbbc91fb03b0312a8284cf9ac89
Carlos D. Di Matteo <carlosdima97@gmail.com>
2024-10-07 19:18:33 +0200

    cleanup

--- script/deployAssets-sepolia.s.sol
+++ script/deployAssets-sepolia.s.sol
@@ -18,10 +18,10 @@ import { CollarOwnedERC20 } from "../test/utils/CollarOwnedERC20.sol";
 contract DeployArbitrumSepoliaAssets is Script {
     using SafeERC20 for IERC20;
 
-    uint constant chainId = 421_614; // Arbitrum Sepolia
-    address constant uniswapV3FactoryAddress = 0x248AB79Bbb9bC29bB72f7Cd42F17e054Fc40188e; // Arbitrum Sepolia UniswapV3Factory
+    // uint constant chainId = 421_614; // Arbitrum Sepolia
+    address constant uniswapV3FactoryAddress = 0x1F98431c8aD98523631AE4a59f267346ea31F984; // Arbitrum Sepolia UniswapV3Factory
 
-    IV3SwapRouter constant SWAP_ROUTER = IV3SwapRouter(0x101F443B4d1b059569D643917553c771E1b9663E);
+    IV3SwapRouter constant SWAP_ROUTER = IV3SwapRouter(0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45);
     uint24 constant FEE_TIER = 3000;
     uint constant INITIAL_MINT_AMOUNT = 1_000_000_000 ether; // 1 billion tokens
 
@@ -32,7 +32,7 @@ contract DeployArbitrumSepoliaAssets is Script {
     }
 
     INonfungiblePositionManager constant POSITION_MANAGER =
-        INonfungiblePositionManager(0x6b2937Bde17889EDCf8fbD8dE31C3C2a70Bc4d65);
+        INonfungiblePositionManager(0xC36442b4a4522E871399CD717aBDD847Ab11FE88);
 
     struct Asset {
         string symbol;
@@ -52,7 +52,7 @@ contract DeployArbitrumSepoliaAssets is Script {
     mapping(string => mapping(string => address)) public deployedPools;
 
     function run() external {
-        require(block.chainid == chainId, "Wrong chain");
+        // require(block.chainid == chainId, "Wrong chain");
 
         uint deployerPrivateKey = vm.envUint("PRIVKEY_DEV_DEPLOYER");
         address deployer = vm.addr(deployerPrivateKey);
@@ -75,7 +75,7 @@ contract DeployArbitrumSepoliaAssets is Script {
         assets.push(Asset("USDC", 6));
         // assets.push(Asset("USDT", 6));
         assets.push(Asset("wETH", 18));
-        // assets.push(Asset("wBTC", 8));
+        assets.push(Asset("wBTC", 8));
         // assets.push(Asset("wPOL", 18));
         // assets.push(Asset("wstETH", 18));
         // assets.push(Asset("weETH", 18));
@@ -88,7 +88,7 @@ contract DeployArbitrumSepoliaAssets is Script {
         // assetPairs.push(AssetPair("coll", "cash", 10_311_236_408_134_789_464_653, 8_946_112_041_115));
         assetPairs.push(AssetPair("wETH", "USDC", 1 ether, 2462e6));
         // assetPairs.push(AssetPair("wETH", "USDT", 11_312_811_541_194_607_057_025, 9_192_731_529_757));
-        // assetPairs.push(AssetPair("wBTC", "USDC", 328e8, 20_025_613_600_000));
+        assetPairs.push(AssetPair("wBTC", "USDC", 1e8, 61_000e6));
         // assetPairs.push(AssetPair("wBTC", "USDT", 328e8, 20_025_613_600_000));
         // assetPairs.push(AssetPair("wPOL", "USDC", 1_315_000 ether, 20_027_450_000_000));
         // assetPairs.push(AssetPair("wPOL", "USDT", 1_315_000 ether, 20_027_450_000_000));
@@ -144,8 +144,8 @@ contract DeployArbitrumSepoliaAssets is Script {
         CollarOwnedERC20 cashAsset = CollarOwnedERC20(deployedAssets[pair.cashSymbol]);
         CollarOwnedERC20 collateralAsset = CollarOwnedERC20(deployedAssets[pair.collateralSymbol]);
 
-        cashAsset.mint(deployer, pair.amountCashDesired * 10);
-        collateralAsset.mint(deployer, pair.amountCollDesired * 10);
+        cashAsset.mint(deployer, pair.amountCashDesired);
+        collateralAsset.mint(deployer, pair.amountCollDesired);
 
         // Approve tokens
         collateralAsset.approve(address(POSITION_MANAGER), type(uint).max);
@@ -156,18 +156,15 @@ contract DeployArbitrumSepoliaAssets is Script {
             ? (pair.amountCollDesired, pair.amountCashDesired)
             : (pair.amountCashDesired, pair.amountCollDesired);
         // uint160 sqrtPriceX96 = 79_228_162_514_264_337_593_543_950_336; // 1:1 price
-        // Calculate the price (token1 per token0)
-        uint price = amount1Desired * (1e18) / amount0Desired;
-        // Convert price to sqrtPriceX96 format
-        uint160 sqrtPriceX96 = encodePriceToSqrtPriceX96(price);
+        uint160 sqrtPriceX96 = getSqrtPriceX96ByAmounts(amount0Desired, amount1Desired);
         IUniswapV3Pool(poolAddress).initialize(sqrtPriceX96);
         (, int24 currentTick,,,,,) = IUniswapV3Pool(poolAddress).slot0();
         int24 tickSpacing = IUniswapV3Pool(poolAddress).tickSpacing();
         console.log("current tick");
         console.logInt(currentTick);
-
-        int24 tickLower = (currentTick - tickSpacing * 10) / tickSpacing * tickSpacing;
-        int24 tickUpper = (currentTick + tickSpacing * 10) / tickSpacing * tickSpacing;
+        // doesnt seem like changing this multiplier (50) does anything regarding price range
+        int24 tickLower = (currentTick - tickSpacing * 50) / tickSpacing * tickSpacing;
+        int24 tickUpper = (currentTick + tickSpacing * 50) / tickSpacing * tickSpacing;
         INonfungiblePositionManager.MintParams memory params = INonfungiblePositionManager.MintParams({
             token0: token0,
             token1: token1,
@@ -183,6 +180,18 @@ contract DeployArbitrumSepoliaAssets is Script {
         });
 
         (, uint128 liquidity, uint amount0, uint amount1) = POSITION_MANAGER.mint(params);
+        // Add more liquidity
+        // for (uint i = 0; i < 1; i++) {
+        //     params.amount0Desired = amount0Desired * 10;
+        //     params.amount1Desired = amount1Desired * 10;
+
+        //     (, liquidity, amount0, amount1) = POSITION_MANAGER.mint(params);
+
+        //     console.log("Additional liquidity added (iteration", i + 1, "):");
+        //     console.log("Liquidity:", liquidity);
+        //     console.log("Amount0:", amount0);
+        //     console.log("Amount1:", amount1);
+        // }
         (sqrtPriceX96, currentTick,,,,,) = IUniswapV3Pool(poolAddress).slot0();
         console.log("current tick after mint");
         console.logInt(currentTick);
@@ -213,39 +222,9 @@ contract DeployArbitrumSepoliaAssets is Script {
         }
     }
 
-    // equation is
-    /**
-     * token1/token0 = ((sqrtPriceX96 / 2**96) **2 ) / ( 10**token1 decimals / 10**token0 decimals)
-     *         sqrtPriceX96 = sqrt((token1 / token0) * (10^token1decimals / 10^token0decimals)) * 2^96
-     */
-    function calculateSqrtPriceX96(
-        uint amountCash,
-        uint amountColl,
-        uint8 cashDecimals,
-        uint8 collDecimals,
-        bool isToken0Cash
-    ) public view returns (uint160 sqrtPriceX96) {
-        require(amountCash > 0 && amountColl > 0, "Amounts must be greater than zero");
-        console.log("isToken0Cash", isToken0Cash);
-        uint token0 = isToken0Cash ? amountCash : amountColl;
-        uint token1 = isToken0Cash ? amountColl : amountCash;
-        uint8 token0Decimals = isToken0Cash ? cashDecimals : collDecimals;
-        uint8 token1Decimals = isToken0Cash ? collDecimals : cashDecimals;
-
-        // Calculate price = (token1 / token0) * (10^(token1decimals - token0decimals))
-        // We'll use a higher precision to avoid loss of significant digits
-        uint price;
-        if (token1Decimals >= token0Decimals) {
-            price = (token1 * (10 ** (token1Decimals - token0Decimals) * (1 << 192))) / token0;
-        } else {
-            price = (token1 * (1 << 192)) / (token0 * (10 ** (token0Decimals - token1Decimals)));
-        }
-
-        // Calculate sqrt(price) * 2^96
-        sqrtPriceX96 = uint160(Math.sqrt(price));
-
-        console.log("sqrtPriceX96", sqrtPriceX96);
-        console.log("price", price);
+    function getSqrtPriceX96ByAmounts(uint amount0, uint amount1) public pure returns (uint160) {
+        uint price = amount1 * (1e18) / amount0;
+        return encodePriceToSqrtPriceX96(price);
     }
 
     function encodePriceToSqrtPriceX96(uint price) private pure returns (uint160) {
@@ -255,7 +234,7 @@ contract DeployArbitrumSepoliaAssets is Script {
     function verifySwapAmount(AssetPair memory pair, address deployer) internal {
         CollarOwnedERC20 cashAsset = CollarOwnedERC20(deployedAssets[pair.cashSymbol]);
         CollarOwnedERC20 collateralAsset = CollarOwnedERC20(deployedAssets[pair.collateralSymbol]);
-        uint amountIn = 1 ether;
+        uint amountIn = 10 ** collateralAsset.decimals();
         // Approve the router to spend tokens
         collateralAsset.approve(address(SWAP_ROUTER), amountIn);
 
