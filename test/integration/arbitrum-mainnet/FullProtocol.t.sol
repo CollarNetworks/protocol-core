961d4ae6b9c05d798522a29e2b87f63e77d8138a
Carlos D. Di Matteo <carlosdima97@gmail.com>
2024-10-24 17:26:37 +0200

    add escrow suite to full protocol tests

--- test/integration/arbitrum-mainnet/FullProtocol.t.sol
+++ test/integration/arbitrum-mainnet/FullProtocol.t.sol
@@ -60,6 +60,22 @@ contract ArbitrumMainnetFullProtocolForkTest is Test {
         loansTest.testOpenAndCloseLoan();
         loansTest.testRollLoan();
         loansTest.testFullLoanLifecycle();
+
         console.log("Full integration test complete");
+    }
+
+    function testEscrowLoans() public {
+        // need to select fork and fund wallets since loans test suite creates a fork (cause it should run independently) we
+        // need to make sure its running in the one from  this contract
+        vm.selectFork(forkId);
+        LoansForkTest loansTest = new LoansForkTest();
+        loansTest.setForkId(forkId);
+        loansTest.setUp();
+        loansTest.testOpenEscrowLoan();
+        loansTest.testOpenAndCloseEscrowLoan();
+        loansTest.testCloseEscrowLoanAfterGracePeriod();
+        loansTest.testCloseEscrowLoanWithPartialLateFees();
+        loansTest.setUp();
+        loansTest.testRollEscrowLoanBetweenSuppliers();
     }
 }
