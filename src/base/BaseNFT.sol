67abaf37506e806fcebac42a8ef4b8ef980bc0d3
artdgn <arthurdgn@gmail.com>
2024-10-24 17:21:43 +1100

    base managed and nft docs

--- src/base/BaseNFT.sol
+++ src/base/BaseNFT.sol
@@ -6,7 +6,12 @@ import { ERC721 } from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import { BaseManaged, ConfigHub } from "./BaseManaged.sol";
 
 /**
- * @custom:security-contact security@collarprotocol.xyz
+ * @title BaseNFT
+ * @notice Base contract for NFTs in the Collar Protocol. It provides the admin functionality
+ * from BaseManaged, and ensures token transfers are paused when the contract is paused.
+ *
+ * @dev All contracts that inherit from BaseManaged must call `_setConfigHub` to set the
+ * ConfigHub address.
  */
 abstract contract BaseNFT is BaseManaged, ERC721 {
     // ----- State ----- //
@@ -19,11 +24,11 @@ abstract contract BaseNFT is BaseManaged, ERC721 {
 
     // ----- INTERNAL MUTATIVE ----- //
 
-    // @dev from ERC721Pausable, to allow pausing transfers
+    // @dev this pauses transfers (as implemented in ERC721Pausable)
     function _update(address to, uint tokenId, address auth)
         internal
         override
-        whenNotPaused // @dev pauses transfers
+        whenNotPaused
         returns (address)
     {
         return super._update(to, tokenId, auth);
