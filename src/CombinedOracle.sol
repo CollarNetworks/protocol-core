563530c6426b4cad28a1f479b6936653ff1788d4
artdgn <arthurdgn@gmail.com>
2024-11-22 19:49:44 +1100

    add docs

--- src/CombinedOracle.sol
+++ src/CombinedOracle.sol
@@ -4,19 +4,31 @@ pragma solidity 0.8.22;
 import { BaseTakerOracle, ITakerOracle } from "./base/BaseTakerOracle.sol";
 
 /**
- * TODO: docs
+ * @title CombinedOracle
+ * @custom:security-contact security@collarprotocol.xyz
+ * @notice Combines two other oracles that use a common asset into one
  *
- * Reference implementation:
+ * Key Assumptions:
+ * - Assets and intermediate assets are chosen such that resulting precision is satisfactory
+ * - Combined price deviations (in case of CL oracles are taken into account)
+ * - The sequencer feed (on L2 networks) on the the leaf oracles is checked (not checked here).
+ *
+ * @dev reference implementation:
  * https://github.com/euler-xyz/euler-price-oracle/blob/0572b45f6096f42f290b7cf7df584226815bfa52/src/adapter/CrossAdapter.sol
  */
 contract CombinedOracle is BaseTakerOracle {
     string public constant VERSION = "0.2.0";
 
+    /// @notice first oracle to be combined (A -> B)
     ITakerOracle public immutable oracle_1;
+    /// @notice second oracle to be combined (C -> D)
     ITakerOracle public immutable oracle_2;
+    /// @notice whether the first oracle's direction is reversed in the path
     bool public immutable invert_1;
+    /// @notice whether the second oracle's direction is reversed in the path
     bool public immutable invert_2;
 
+    // internal caching of fixed amounts
     uint internal immutable base1Amount;
     uint internal immutable base2Amount;
     uint internal immutable quote1Amount;
@@ -36,15 +48,17 @@ contract CombinedOracle is BaseTakerOracle {
         invert_2 = _invert_2;
 
         // check that all assets match: base, cross, and quote
-        // assume 1 not inverted and invert if needed
+        // assume 1 not inverted and invert if needed. If inverted, A->B turns into B->A
         (address base, address cross_1) = (oracle_1.baseToken(), oracle_1.quoteToken());
         if (invert_1) (base, cross_1) = (cross_1, base);
 
-        // assume 2 not inverted and invert if needed
+        // assume 2 not inverted and invert if needed. If inverted, C->D turns into D->C
         (address cross_2, address quote) = (oracle_2.baseToken(), oracle_2.quoteToken());
         if (invert_2) (quote, cross_2) = (cross_2, quote);
 
-        // check all match passed in tokens and implied cross token
+        // check all match passed in tokens and implied cross token.
+        // For example, if no inversion of was done for 1: A->B and 2: C->D
+        // We expected that A == baseToken, D == quoteToken, and B == C
         require(base == baseToken, "CombinedOracle: base argument mismatch");
         require(quote == quoteToken, "CombinedOracle: quote argument mismatch");
         require(cross_1 == cross_2, "CombinedOracle: cross token mismatch");
@@ -55,6 +69,7 @@ contract CombinedOracle is BaseTakerOracle {
     }
 
     /// @notice Current price of a unit of base tokens (i.e. 10**baseToken.decimals()) in quote tokens.
+    /// For a path combination of (A->B, B->C), it quotes the price of A->C, of one unit of A in C units
     function currentPrice() external view override returns (uint) {
         // going from oracle 1 to 2
         uint price1 = invert_1 ? oracle_1.inversePrice() : oracle_1.currentPrice();
@@ -66,6 +81,7 @@ contract CombinedOracle is BaseTakerOracle {
     }
 
     /// @notice Current price of a unit of quote tokens (i.e. 10**quoteToken.decimals()) in base tokens.
+    /// For a path combination of (A->B, B->C), it quotes the price of C->A, of one unit of C in A units
     function inversePrice() external view returns (uint) {
         // @dev the invert conditionals are flipped because of the reversed direction of trade
         // going from oracle 2 to 1
