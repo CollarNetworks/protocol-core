7a7c7585e9e01ef32bd4f785d689167148b591bd
artdgn <arthurdgn@gmail.com>
2024-11-22 18:15:12 +1100

    add local tests for combo oracle

--- src/CombinedOracle.sol
+++ src/CombinedOracle.sol
@@ -1,9 +1,6 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.22;
 
-import { IERC20Metadata } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import { Strings } from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-
 import { BaseTakerOracle, ITakerOracle } from "./base/BaseTakerOracle.sol";
 
 /**
@@ -19,6 +16,11 @@ contract CombinedOracle is BaseTakerOracle {
     ITakerOracle public immutable oracle_2;
     bool public immutable invert_1;
     bool public immutable invert_2;
+
+    uint internal immutable base1Amount;
+    uint internal immutable base2Amount;
+    uint internal immutable quote1Amount;
+    uint internal immutable quote2Amount;
 
     constructor(
         address _baseToken,
@@ -43,29 +45,34 @@ contract CombinedOracle is BaseTakerOracle {
         if (invert_2) (quote, cross_2) = (cross_2, quote);
 
         // check all match passed in tokens and implied cross token
-        require(base == baseToken, "CombinedChainlinkOracle: base argument mismatch");
-        require(quote == quoteToken, "CombinedChainlinkOracle: quote argument mismatch");
-        require(cross_1 == cross_2, "CombinedChainlinkOracle: cross token mismatch");
+        require(base == baseToken, "CombinedOracle: base argument mismatch");
+        require(quote == quoteToken, "CombinedOracle: quote argument mismatch");
+        require(cross_1 == cross_2, "CombinedOracle: cross token mismatch");
+
+        // cache amounts
+        (base1Amount, quote1Amount) = (oracle_1.baseUnitAmount(), oracle_1.quoteUnitAmount());
+        (base2Amount, quote2Amount) = (oracle_2.baseUnitAmount(), oracle_2.quoteUnitAmount());
     }
 
     /// @notice Current price of a unit of base tokens (i.e. 10**baseToken.decimals()) in quote tokens.
     function currentPrice() external view override returns (uint) {
         // going from oracle 1 to 2
         uint price1 = invert_1 ? oracle_1.inversePrice() : oracle_1.currentPrice();
-        uint divisor1 = invert_1 ? oracle_1.baseUnitAmount() : oracle_1.quoteUnitAmount();
+        uint divisor1 = invert_1 ? base1Amount : quote1Amount;
         uint price2 = invert_2 ? oracle_2.inversePrice() : oracle_2.currentPrice();
-        // 1: ETH/USD_18, 2: inv USDC/USD_18.
+        // 1: ETH/USD_18, 2: inv USDC/USD_18. currentPrice() is for unit of ETH in USDC
         // p1=3000e18, d1=1e18, p2=1e6 -> 3000e18 * 1e6 / 1e18 -> 3000e6
         return price1 * price2 / divisor1;
     }
 
     /// @notice Current price of a unit of quote tokens (i.e. 10**quoteToken.decimals()) in base tokens.
     function inversePrice() external view returns (uint) {
-        // going from oracle 2 to 1, the inverts are flipped
+        // @dev the invert conditionals are flipped because of the reversed direction of trade
+        // going from oracle 2 to 1
         uint price2 = invert_2 ? oracle_2.currentPrice() : oracle_2.inversePrice();
-        uint divisor2 = invert_2 ? oracle_2.quoteUnitAmount() : oracle_2.baseUnitAmount();
+        uint divisor2 = invert_2 ? quote2Amount : base2Amount;
         uint price1 = invert_1 ? oracle_1.currentPrice() : oracle_1.inversePrice();
-        // 1: ETH/USD_18, 2: inv USDC/USD_18.
+        // 1: ETH/USD_18, 2: inv USDC/USD_18. inversePrice() is for unit of USDC in ETH
         // p2=1e18, d2=1e18, p1=(1/3000)e18 -> 1e18 * (1/3000)e18 / 1e18 -> (1/3000)e18
         return price2 * price1 / divisor2;
     }
