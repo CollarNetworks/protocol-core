493ce91d74f26c324199a3dbf8f462837e04305e
artdgn <arthurdgn@gmail.com>
2025-02-20 15:18:24 +1100

    add new fork tests, refactor

---
+++ test/integration/full-protocol/Loans.fork.arbi.t.sol
@@ -0,0 +1,194 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.22;
+
+import "forge-std/Test.sol";
+import { Const } from "../../../script/utils/Const.sol";
+import { DeployArbitrumMainnet, DeployArbitrumSepolia } from "../../../script/deploy/deploy-protocol.s.sol";
+import {
+    AcceptOwnershipArbiMainnet,
+    AcceptOwnershipArbiSepolia
+} from "../../../script/deploy/accept-ownership.s.sol";
+
+import { BaseAssetPairForkTest_ScriptTest, BaseDeployer } from "./Loans.fork.opbase.t.sol";
+
+contract WETHUSDC_ArbiMain_LoansForkTest is BaseAssetPairForkTest_ScriptTest {
+    function deployFullProtocol()
+        internal
+        virtual
+        override
+        returns (BaseDeployer.DeploymentResult memory result)
+    {
+        result = (new DeployArbitrumMainnet()).run(fakeArtifactsName);
+
+        // accept ownership of the new deployment from the owner (via broadcast instead of prank)
+        (new AcceptOwnershipArbiMainnet()).run(fakeArtifactsName);
+    }
+
+    function _setTestValues() internal virtual override {
+        RPCEnvVarsBase = "ARBITRUM_MAINNET";
+
+        owner = Const.ArbiMain_owner;
+
+        // config params
+        protocolFeeAPR = 90;
+        protocolFeeRecipient = Const.ArbiMain_feeRecipient;
+        pauseGuardians.push(Const.ArbiMain_deployerAcc);
+
+        // @dev all pairs must be tested, so if this number is increased, test classes must be added
+        expectedNumPairs = 3;
+
+        // set up all the variables for this pair
+        expectedPairIndex = 0;
+        underlying = Const.ArbiMain_WETH;
+        cashAsset = Const.ArbiMain_USDC;
+        oracleDescription = "Comb(CL(ETH / USD)|inv(CL(USDC / USD)))";
+
+        offerAmount = 100_000e6;
+        underlyingAmount = 1 ether;
+        minLoanAmount = 0.3e6; // arbitrary low value
+        rollFee = 100e6;
+        rollDeltaFactor = 10_000;
+        bigCashAmount = 1_000_000e6;
+        bigUnderlyingAmount = 1000 ether;
+        swapPoolFeeTier = 500;
+
+        slippage = 100; // 1%
+        callstrikeToUse = 11_000;
+
+        expectedOraclePrice = 3_000_000_000;
+    }
+}
+
+contract WETHUSDC_ArbiMain_LoansForkTest_noExport is WETHUSDC_ArbiMain_LoansForkTest {
+    function _setTestValues() internal virtual override {
+        super._setTestValues();
+        testMode = TestMode.deployOnly;
+    }
+}
+
+contract ArbiMain_LoansForkTest_LatestBlock is WETHUSDC_ArbiMain_LoansForkTest {
+    function _setTestValues() internal virtual override {
+        super._setTestValues();
+        forceLatestBlock = true;
+    }
+}
+
+contract WETHUSDT_ArbiMain_LoansForkTest is WETHUSDC_ArbiMain_LoansForkTest {
+    function _setTestValues() internal virtual override {
+        super._setTestValues();
+        expectedPairIndex = 1;
+        underlying = Const.ArbiMain_WETH;
+        cashAsset = Const.ArbiMain_USDT;
+        oracleDescription = "Comb(CL(ETH / USD)|inv(CL(USDT / USD)))";
+    }
+}
+
+contract WBTCUSDT_ArbiMain_LoansForkTest is WETHUSDC_ArbiMain_LoansForkTest {
+    function _setTestValues() internal virtual override {
+        super._setTestValues();
+
+        expectedPairIndex = 2;
+        underlying = Const.ArbiMain_WBTC;
+        cashAsset = Const.ArbiMain_USDT;
+        oracleDescription = "Comb(CL(WBTC / USD)|inv(CL(USDT / USD)))";
+        underlyingAmount = 0.1e8;
+        bigUnderlyingAmount = 100e8;
+
+        callstrikeToUse = 10_500;
+
+        expectedOraclePrice = 90_000_000_000;
+    }
+}
+
+// Arbi Sepolia
+
+contract WETHUSDC_ArbiSep_LoansForkTest is WETHUSDC_ArbiMain_LoansForkTest {
+    function _setTestValues() internal virtual override {
+        super._setTestValues();
+
+        RPCEnvVarsBase = "ARBITRUM_SEPOLIA";
+
+        owner = Const.ArbiSep_owner;
+        protocolFeeRecipient = Const.ArbiSep_feeRecipient;
+        delete pauseGuardians;
+        pauseGuardians.push(Const.ArbiSep_deployerAcc);
+
+        // @dev all pairs must be tested, so if this number is increased, test classes must be added
+        expectedNumPairs = 2;
+
+        // set up all the variables for this pair
+        expectedPairIndex = 0;
+        underlying = Const.ArbiSep_tWETH;
+        cashAsset = Const.ArbiSep_tUSDC;
+        oracleDescription = "Comb(CL(TWAPMock(ETH / USD))|inv(CL(FixedMock(USDC / USD))))";
+
+        expectedOraclePrice = 3_000_000_000;
+    }
+
+    function deployFullProtocol()
+        internal
+        virtual
+        override
+        returns (BaseDeployer.DeploymentResult memory result)
+    {
+        result = (new DeployArbitrumSepolia()).run(fakeArtifactsName);
+
+        // accept ownership of the new deployment from the owner (via broadcast instead of prank)
+        (new AcceptOwnershipArbiSepolia()).run(fakeArtifactsName);
+    }
+}
+
+contract ArbiSep_LoansForkTest_LatestBlock is WETHUSDC_ArbiSep_LoansForkTest {
+    function _setTestValues() internal virtual override {
+        super._setTestValues();
+        forceLatestBlock = true;
+    }
+}
+
+contract WBTCUSDC_ArbiSep_LoansForkTest is WETHUSDC_ArbiSep_LoansForkTest {
+    function _setTestValues() internal virtual override {
+        super._setTestValues();
+
+        // set up all the variables for this pair
+        expectedPairIndex = 1;
+        underlying = Const.ArbiSep_tWBTC;
+        cashAsset = Const.ArbiSep_tUSDC;
+        oracleDescription = "Comb(CL(TWAPMock(BTC / USD))|inv(CL(FixedMock(USDC / USD))))";
+
+        underlyingAmount = 0.1e8;
+        bigUnderlyingAmount = 100e8;
+
+        expectedOraclePrice = 90_000_000_000;
+    }
+}
+
+contract WETHUSDC_ArbiSep_LoansForkTest_noExport is WETHUSDC_ArbiSep_LoansForkTest {
+    function _setTestValues() internal virtual override {
+        super._setTestValues();
+        testMode = TestMode.deployOnly;
+    }
+}
+
+contract WBTCUSDC_ArbiSep_LoansForkTest_noExport is WBTCUSDC_ArbiSep_LoansForkTest {
+    function _setTestValues() internal virtual override {
+        super._setTestValues();
+        testMode = TestMode.deployOnly;
+    }
+}
+
+////// load existing sepolia deployment
+contract WETHUSDC_ArbiSep_LoansForkTest_NoDeploy is ArbiSep_LoansForkTest_LatestBlock {
+    function _setTestValues() internal virtual override {
+        super._setTestValues();
+        testMode = TestMode.noDeploy;
+        realArtifactsName = Const.ArbiSep_artifactsName;
+    }
+}
+
+contract WBTCUSDC_ArbiSep_LoansForkTest_NoDeploy is WBTCUSDC_ArbiSep_LoansForkTest {
+    function _setTestValues() internal virtual override {
+        super._setTestValues();
+        testMode = TestMode.noDeploy;
+        realArtifactsName = Const.ArbiSep_artifactsName;
+    }
+}
---
+++ test/integration/full-protocol/Loans.fork.opbase.t.sol
@@ -0,0 +1,235 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.22;
+
+import "forge-std/Test.sol";
+import { Const } from "../../../script/utils/Const.sol";
+import { DeploymentArtifactsLib } from "../../../script/libraries/DeploymentArtifacts.sol";
+import { BaseAssetPairForkTest, ConfigHub } from "./BaseAssetPairForkTest.sol";
+import { BaseDeployer } from "../../../script/libraries/BaseDeployer.sol";
+import { DeployOPBaseMainnet, DeployOPBaseSepolia } from "../../../script/deploy/deploy-protocol.s.sol";
+import {
+    AcceptOwnershipOPBaseSepolia,
+    AcceptOwnershipOPBaseMainnet
+} from "../../../script/deploy/accept-ownership.s.sol";
+
+abstract contract BaseAssetPairForkTest_ScriptTest is BaseAssetPairForkTest {
+    string fakeArtifactsName = "collar_protocol_fork_deployment";
+
+    enum TestMode {
+        // deploys everything, saves artifacts, loads artifacts
+        deploySaveLoad,
+        // deploys everything, no artifacts
+        deployOnly,
+        // only loads an existing deployment
+        noDeploy
+    }
+
+    TestMode testMode = TestMode.deploySaveLoad;
+    bool forceLatestBlock = false; // use latest block always
+
+    string RPCEnvVarsBase; // eg OPBASE_MAINNET
+    string realArtifactsName; // needed for noDeploy mode
+
+    function getDeployedContracts()
+        internal
+        virtual
+        override
+        returns (ConfigHub hub, BaseDeployer.AssetPairContracts[] memory pairs)
+    {
+        setupNewFork();
+
+        if (testMode == TestMode.noDeploy) {
+            return DeploymentArtifactsLib.loadHubAndAllPairs(vm, realArtifactsName);
+        } else {
+            BaseDeployer.DeploymentResult memory result = deployFullProtocol();
+            if (testMode == TestMode.deploySaveLoad) {
+                return DeploymentArtifactsLib.loadHubAndAllPairs(vm, fakeArtifactsName);
+            } else {
+                return (result.configHub, result.assetPairContracts);
+            }
+        }
+    }
+
+    function setupNewFork() internal virtual {
+        string memory rpc = vm.envString(string.concat(RPCEnvVarsBase, "_RPC"));
+        // if we are in development we want to fix the block to reduce the time it takes to run the tests
+        if (forceLatestBlock || !vm.envBool(string.concat("FIX_BLOCK_", RPCEnvVarsBase))) {
+            vm.createSelectFork(rpc);
+        } else {
+            vm.createSelectFork(rpc, vm.envUint(string.concat("BLOCK_NUMBER_", RPCEnvVarsBase)));
+        }
+    }
+
+    // abstract
+
+    function deployFullProtocol() internal virtual returns (BaseDeployer.DeploymentResult memory);
+}
+
+// OPBase mainnet
+
+contract WETHUSDC_OPBaseMain_LoansForkTest is BaseAssetPairForkTest_ScriptTest {
+    function deployFullProtocol()
+        internal
+        virtual
+        override
+        returns (BaseDeployer.DeploymentResult memory result)
+    {
+        result = (new DeployOPBaseMainnet()).run(fakeArtifactsName);
+
+        // accept ownership of the new deployment from the owner (via broadcast instead of prank)
+        (new AcceptOwnershipOPBaseMainnet()).run(fakeArtifactsName);
+    }
+
+    function _setTestValues() internal virtual override {
+        RPCEnvVarsBase = "OPBASE_MAINNET";
+
+        owner = Const.OPBaseMain_owner;
+
+        // config params
+        protocolFeeAPR = 90;
+        protocolFeeRecipient = Const.OPBaseMain_feeRecipient;
+        pauseGuardians.push(Const.OPBaseMain_deployerAcc);
+
+        // @dev all pairs must be tested, so if this number is increased, test classes must be added
+        expectedNumPairs = 2;
+
+        // set up all the variables for this pair
+        expectedPairIndex = 0;
+        underlying = Const.OPBaseMain_WETH;
+        cashAsset = Const.OPBaseMain_USDC;
+        oracleDescription = "Comb(CL(ETH / USD)|inv(CL(USDC / USD)))";
+
+        offerAmount = 100_000e6;
+        underlyingAmount = 1 ether;
+        minLoanAmount = 0.3e6; // arbitrary low value
+        rollFee = 100e6;
+        rollDeltaFactor = 10_000;
+        bigCashAmount = 1_000_000e6;
+        bigUnderlyingAmount = 1000 ether;
+        swapPoolFeeTier = 500;
+
+        slippage = 100; // 1%
+        callstrikeToUse = 11_000;
+
+        expectedOraclePrice = 3_000_000_000;
+    }
+}
+
+contract WETHUSDC_OPBaseMain_LoansForkTest_noExport is WETHUSDC_OPBaseMain_LoansForkTest {
+    function _setTestValues() internal virtual override {
+        super._setTestValues();
+        testMode = TestMode.deployOnly;
+    }
+}
+
+contract WETHUSDC_OPBaseMain_LoansForkTest_NoDeploy is WETHUSDC_OPBaseMain_LoansForkTest {
+    function _setTestValues() internal virtual override {
+        super._setTestValues();
+        forceLatestBlock = true;
+        testMode = TestMode.noDeploy;
+        realArtifactsName = Const.OPBaseMain_artifactsName;
+    }
+}
+
+contract CBBTCUSDC_OPBaseMain_LoansForkTest is WETHUSDC_OPBaseMain_LoansForkTest {
+    function _setTestValues() internal virtual override {
+        super._setTestValues();
+
+        // @dev all pairs must be tested, so if this number is increased, test classes must be added
+        expectedNumPairs = 2;
+
+        // set up all the variables for this pair
+        expectedPairIndex = 1;
+        underlying = Const.OPBaseMain_cbBTC;
+        cashAsset = Const.OPBaseMain_USDC;
+        oracleDescription = "Comb(CL(cbBTC / USD)|inv(CL(USDC / USD)))";
+
+        underlyingAmount = 0.1e8;
+        bigUnderlyingAmount = 100e8;
+
+        expectedOraclePrice = 90_000_000_000;
+    }
+}
+
+contract CBBTCUSDC_OPBaseMain_LoansForkTest_NoDeploy is CBBTCUSDC_OPBaseMain_LoansForkTest {
+    function _setTestValues() internal virtual override {
+        super._setTestValues();
+        forceLatestBlock = true;
+        testMode = TestMode.noDeploy;
+        realArtifactsName = Const.OPBaseMain_artifactsName;
+    }
+}
+
+//// OPBase sep
+contract TWETHTUSDC_OPBaseSep_LoansForkTest is WETHUSDC_OPBaseMain_LoansForkTest {
+    function deployFullProtocol()
+        internal
+        virtual
+        override
+        returns (BaseDeployer.DeploymentResult memory result)
+    {
+        // run the deployment and setup
+        result = (new DeployOPBaseSepolia()).run(fakeArtifactsName);
+
+        // accept ownership of the new deployment from the owner (via broadcast instead of prank)
+        (new AcceptOwnershipOPBaseSepolia()).run(fakeArtifactsName);
+    }
+
+    function _setTestValues() internal virtual override {
+        super._setTestValues();
+
+        RPCEnvVarsBase = "OPBASE_SEPOLIA";
+
+        owner = Const.OPBaseSep_owner;
+
+        // config params
+        protocolFeeRecipient = Const.OPBaseSep_feeRecipient;
+        delete pauseGuardians;
+        pauseGuardians.push(Const.OPBaseSep_deployerAcc);
+
+        // @dev all pairs must be tested, so if this number is increased, test classes must be added
+        expectedNumPairs = 2;
+
+        // set up all the variables for this pair
+        expectedPairIndex = 0;
+        underlying = Const.OPBaseSep_tWETH;
+        cashAsset = Const.OPBaseSep_tUSDC;
+        oracleDescription = "Comb(CL(TWAPMock(ETH / USD))|inv(CL(FixedMock(USDC / USD))))";
+
+        expectedOraclePrice = 3_500_000_000; // 3.5k in 1e6
+    }
+}
+
+contract TWETHTUSDC_OPBaseSep_LoansForkTest_NoDeploy is TWETHTUSDC_OPBaseSep_LoansForkTest {
+    function _setTestValues() internal virtual override {
+        super._setTestValues();
+        forceLatestBlock = true;
+        testMode = TestMode.noDeploy;
+        realArtifactsName = Const.OPBaseSep_artifactsName;
+    }
+}
+
+contract TWBTCTUSDC_OPBaseSep_LoansForkTest is TWETHTUSDC_OPBaseSep_LoansForkTest {
+    function _setTestValues() internal virtual override {
+        super._setTestValues();
+        // set up all the variables for this pair
+        expectedPairIndex = 1;
+        underlying = Const.OPBaseSep_tWBTC;
+        cashAsset = Const.OPBaseSep_tUSDC;
+        oracleDescription = "Comb(CL(TWAPMock(BTC / USD))|inv(CL(FixedMock(USDC / USD))))";
+
+        underlyingAmount = 0.1e8;
+        bigUnderlyingAmount = 100e8;
+
+        expectedOraclePrice = 100_000_000_000; // 100k in 1e6
+    }
+}
+
+contract TWBTCTUSDC_OPBaseSep_LoansForkTest_NoDeploy is TWBTCTUSDC_OPBaseSep_LoansForkTest {
+    function _setTestValues() internal virtual override {
+        super._setTestValues();
+        forceLatestBlock = true;
+        testMode = TestMode.noDeploy;
+        realArtifactsName = Const.OPBaseSep_artifactsName;
+    }
+}
